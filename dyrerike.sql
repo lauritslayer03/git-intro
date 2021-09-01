c32d421707fe808176c10510ef333697dcfe5747
lauritslayer03 <89899291+lauritslayer03@users.noreply.github.com>
2021-09-01 13:01:44 +0200

    Create dyrerike.sql

---
+++ dyrerike.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Somalikatt (
+    Rike varchar(255),
+    Rekke varchar(255),
+    Klasse varchar(255),
+    Orden varchar(255),
+    Familie varchar(255), 
+    Slekt varchar(255),
+    Art varchar(255)
+);
+
